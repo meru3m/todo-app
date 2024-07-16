32a0a28a2690dbea22fbd759794ca214dcfb612d
meru3m <gustokomagprogram@gmail.com>
2024-07-16 14:13:05 +0800

    Implemented Edit Button

--- gui.py
+++ gui.py
@@ -3,11 +3,13 @@ import FreeSimpleGUI as sg
 
 # Create the elements that go inside the window
 label = sg.Text("Type in a to-do")  # Create a text label
-input_box = sg.InputText(tooltip="Enter todo", key="todo")  # Create a text box with a tooltip
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")  # Create an "Add" button
+list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events = True, size=[45,10])
+edit_button = sg.Button("Edit")
 
 # Define the layout of the window
-layout = [[label], [input_box, add_button]]  # Arrange the elements in the window
+layout = [[label], [input_box, add_button], [list_box, edit_button]]  # Arrange the elements in the window
 
 # Create the window with a title and the defined layout
 window = sg.Window("My To-Do App", layout, font=('Montserrat'))
@@ -25,6 +27,22 @@ while True:
         new_todo = values['todo'] + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        
+    elif event == "Edit":
+        todo_to_edit = values['todos'][0]  # Get the first selected item from the Listbox
+        new_todo =values['todo'] + "\n"
+
+        todos = functions.get_todos()
+        index = todos.index(todo_to_edit)
+        todos[index] = new_todo
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        
+    elif event == "todos":
+        #values here is the dictionary, values=['todos] ('todos' is the key) will return the list (value = selected todo in the list) index 0 is the index of the only item(string) in the list
+        window['todo'].update(value=values['todos'][0])
+        
     elif event == sg.WIN_CLOSED:
         break
         
