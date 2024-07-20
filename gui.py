0681fd27c2639e1c43a8ddecf10739b6acc4647d
meru3m <gustokomagprogram@gmail.com>
2024-07-20 20:53:04 +0800

    implemented complete and exit button

--- gui.py
+++ gui.py
@@ -7,16 +7,19 @@ input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")  # Create an "Add" button
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events = True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
 # Define the layout of the window
-layout = [[label], [input_box, add_button], [list_box, edit_button]]  # Arrange the elements in the window
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [sg.Button("Exit")]]  # Arrange the elements in the window
 
 # Create the window with a title and the defined layout
 window = sg.Window("My To-Do App", layout, font=('Montserrat'))
 
 # Display the window and keep it open until the user interacts with it
 # window.read() in FreeSimpleGUI always return a tuple with exactly two values: event and values
-
 while True:
     event, values = window.read()
     print(event) #add_button
@@ -39,13 +42,22 @@ while True:
         functions.write_todos(todos)
         window['todos'].update(values=todos)
         
+    elif event == "Complete":
+        completed = values['todos'][0]
+        todos = functions.get_todos()
+        todos.remove(completed)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+        window['todo'].update(value='')
+
     elif event == "todos":
         #values here is the dictionary, values=['todos] ('todos' is the key) will return the list (value = selected todo in the list) index 0 is the index of the only item(string) in the list
-        window['todo'].update(value=values['todos'][0])
+        if values['todos']:
+            window['todo'].update(value=values['todos'][0])
+        else:
+            continue
         
-    elif event == sg.WIN_CLOSED:
+    elif event == sg.WIN_CLOSED or event == "Exit":
         break
         
-
-# Close the window when done
 window.close()
